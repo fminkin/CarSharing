a8c5205259d9bb7ee1d4e14c1657fcb07223f71f
f-minkin <f-minkin@yandex-team.ru>
2017-12-21 03:52:12 +0300

    added user info

--- src/carsharing/user/user_info.py
+++ src/carsharing/user/user_info.py
@@ -1,5 +1,7 @@
 
 
 class UserInfo(object):
-    def __init__(self):
-        pass
+    def __init__(self, email, pws, images):
+        self.email = email
+        self.pws = pws
+        self.images = images
--- src/client/main.py
+++ src/client/main.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request
 
+import json
 from src.carsharing.user import UserInfo
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str)
+
+args = parser.parse_args()
+
+with open(args.config) as f:
+    config = json.load(f)
+
+UPLOAD_FOLDER = config["upload_folder"]
+SERVER_ADDRESS = config["server_adress"]
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup_page():
     if request.method == "POST":
-        user_info = UserInfo()
-
         print(request.form['email'])
         print(request.form['psw'])
-        print(request.form)
+        # print(request.files)
+        for i in request.files.getlist('file'):
+            print(i)
+        user_info = UserInfo(request.form['email'], request.form['psw'], 123)
 
     # ImmutableMultiDict([('email', 'huy'), ('psw', '123'), ('psw-repeat', '123')])
     return render_template('signup.html')
@@ -27,4 +42,4 @@ def index_page():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=9889, debug=False)
